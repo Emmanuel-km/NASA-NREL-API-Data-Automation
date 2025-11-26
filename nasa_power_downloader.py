69f2447f95975d0bef41797c7dddeb4c1ca15d78
Emmanuel-km <kiokoemmanuel027@gmail.com>
2025-11-26 21:59:22 +0300

    atleast a loop in the data

--- nasa_power_downloader.py
+++ nasa_power_downloader.py
@@ -3,4 +3,18 @@ import sqlite3
 
 #variables and constants
 Base_url='https://power.larc.nasa.gov/api/temporal/daily/point?'
+parameters='ALLSKY_SFC_SW_DWN,T2M,RH2M,WS10M&community=RE&longitude=-3.7038&latitude=40.4168&start=20200101&end=20241231&format=CSV'
 
+#trying out to get data from the API
+def fetch_nasa_power_data():
+    import requests
+    url = Base_url + parameters
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.text
+        return data
+    else:
+        print("Error fetching data:", response.status_code)
+        return None
+data = fetch_nasa_power_data()
+print(data)
