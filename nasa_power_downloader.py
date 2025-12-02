353a62e792dea3e0b5911d79572ff12d77dc581c
Emmanuel-km <kiokoemmanuel027@gmail.com>
2025-12-02 23:48:56 +0300

    ecapsulating the code in a try except block

--- nasa_power_downloader.py
+++ nasa_power_downloader.py
@@ -9,15 +9,20 @@ Base_url='https://power.larc.nasa.gov/api/temporal/daily/point?'
 parameters='ALLSKY_SFC_SW_DWN,T2M,RH2M,WS10M&community=RE&longitude=-3.7038&latitude=40.4168&start=20200101&end=20241231&format=CSV'
 
 #trying out to get data from the API
+
 def fetch_nasa_power_data():
-    import requests
-    url = Base_url + parameters
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.text
-        return data
-    else:
-        print("Error fetching data:", response.status_code)
+    try:
+        import requests
+        url = Base_url + parameters
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.text
+            return data
+        else:
+            print("Error fetching data:", response.status_code)
+            return None
+    except Exception as e:
+        print("An error occurred:", e)
         return None
 data = fetch_nasa_power_data()
 print(data)
